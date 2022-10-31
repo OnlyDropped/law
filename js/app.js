706bfed7432d89c6e377425040b2b1b5f757f490
OnlyDropped <asheburenkov@yandex.ru>
2022-10-31 20:59:39 +0300

    added new changes

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$('.right').slick({
+  slidesToShow: 3, 
+  slidesToScroll: 1,
+  speed: 1500,
+  arrows: true,
+  easing: 'easeOutElastic',
+  cssEase: 'ease-in-out',
+  rtl: false,
+  dots: true,
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/slick-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icons/slick-next.svg" alt=""></button>'
+
+})
+
