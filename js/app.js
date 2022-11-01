9f7ccac219bd690973ffe4d98b2a715d2f0e0310
OnlyDropped <asheburenkov@yandex.ru>
2022-11-01 17:25:54 +0300

    add new changes in media.scss

--- js/app.js
+++ js/app.js
@@ -8,13 +8,30 @@ $('.right').slick({
   rtl: false,
   dots: true,
   prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/slick-prev.svg" alt=""></button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="img/icons/slick-next.svg" alt=""></button>'
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icons/slick-next.svg" alt=""></button>',
+    responsive: [
+    {
+      breakpoint: 821,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 426,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
 
 })
 
 
 const btn = document.querySelector('.header-nav__btn');
 const nav = document.querySelector('.header-nav__wrap');
+const menu = document.querySelector('.header-nav__wrap');
 
 btn.addEventListener('click', function () {
   btn.classList.toggle('header-nav__btn--active');
